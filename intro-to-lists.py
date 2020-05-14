de3ae3ce97117d8c27982cbafe6925a561eb6f6d
a-powers <awpowers@gmail.com>
2020-05-14 19:33:19 -0400

    Update

--- intro-to-lists.py
+++ intro-to-lists.py
@@ -1,12 +1,24 @@
 # List aka array is a data structure that stores an ordered sequence of objects.
-empty = []
-empty = list()  # use this to convert another data structure to a list
+# empty = []
+# empty = list()  # use this to convert another data structure to a list
 
-sodas = ["Coke", "Pepsi", "Dr. Pepper"]
-print(len(sodas))
+# sodas = ["Coke", "Pepsi", "Dr. Pepper"]
+# print(len(sodas))
 
-quarterly_revenue = [15000, 12000, 9000, 20000]
-print(len(quarterly_revenue))
+# quarterly_revenue = [15000, 12000, 9000, 20000]
+# print(len(quarterly_revenue))
 
-user_settings = [True, False, True, True]
-print(len(user_settings))
+# user_settings = [True, False, True, True]
+# print(len(user_settings))
+
+
+
+# Coding Exercise 14
+
+
+def is_long(fruits = []):
+    if len(fruits) > 5:
+        return True
+    else:
+        return False
+print(is_long(fruits= ["a", "b", "c", "d", "e", "f"]))
