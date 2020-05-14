751b29dca0dc7b52a3939218daf0d4e8788b4e36
a-powers <awpowers@gmail.com>
2020-05-13 21:35:43 -0400

    New

--- intro-to-lists.py
+++ intro-to-lists.py
@@ -1,1 +1,12 @@
-print("eeenie meanie mynie moe")
+# List aka array is a data structure that stores an ordered sequence of objects.
+empty = []
+empty = list()  # use this to convert another data structure to a list
+
+sodas = ["Coke", "Pepsi", "Dr. Pepper"]
+print(len(sodas))
+
+quarterly_revenue = [15000, 12000, 9000, 20000]
+print(len(quarterly_revenue))
+
+user_settings = [True, False, True, True]
+print(len(user_settings))
