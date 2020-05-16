3f8892e2f31f40b9b4dd5092ed8953d4432b779c
a-powers <awpowers@gmail.com>
2020-05-16 19:51:47 -0400

    update

---
+++ slice-multiple-elements-from-a-list.py
@@ -0,0 +1,54 @@
+# print("programming"[3:6])
+
+# muscles = ["Biceps", "Triceps", "Deltoids", "Pectoralis"]
+
+# print(muscles[1:3])
+# print(muscles[1:2])
+# print(muscles[0:])
+# print(muscles[:2])
+# print(muscles[2:5])
+# print(muscles[2:])
+
+# print(muscles[-4:-2])
+# print(muscles[-3:])
+# print(muscles[:-1])
+# print(muscles[1:-1])
+
+# print(muscles[::2])
+# print(muscles[::-2])
+# print(muscles[::-1])
+
+
+
+# # Coding Exercise 16
+
+# def split_in_two(a = list, b = int):
+#     if b % 2 == 0:
+#         return a[2:]
+#     else:
+#         return a[:2]
+# print(split_in_two(a = ["a", "b", "c", "d", "e", "f"], b = 10))
+
+# Coding Exercise 17
+
+nl = [[3, 4, 5], [7, 8, 9], [10, 11, 12]]
+
+def nested_extraction(a: list, b: int):
+    return nl[a][b]
+print(nested_extraction(0, 1))
+    
+
+
+def beginning_and_end(a: list):
+    if a[0] == a[-1]:
+        return True
+   
+print(beginning_and_end(["a", "b", "a"]))
+
+
+words = ["cat", "dog", "rhino"]
+def long_word_in_collection(words:list, b: str):
+    if b in words and len(b) > 4:
+        return True
+
+print(long_word_in_collection(words, "rhino"))
